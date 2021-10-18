08b218d6ea4cb023d8fe95cf433c81c335e7aec6
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-10-18 11:53:07 -0500

    Add project files.

---
+++ TableReservation/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TableReservation.Models
+{
+    public class Customer : IdentityUser
+    {
+
+        [Display(Name = "First Name")]
+        public string FristName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Address")]
+        public string MailAddress { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string C_State { get; set; }
+    }
+}
