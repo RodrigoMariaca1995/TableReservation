11f211d1b72965211fe45e2fe5f3c0b6abd738bd
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-10-25 15:40:29 -0500

    Created reservation page

---
+++ TableReservation/Controllers/ReservationsController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TableReservation.Data;
+using TableReservation.Models;
+
+
+namespace TableReservation.Controllers
+{
+    public class ReservationsController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public ReservationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservations
+        public async Task<IActionResult> Index()
+        {
+            var customer = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var reservations = _context.Reservations.Include(r => r.Customer).AsNoTracking();
+            var curDate = DateTime.Now;
+
+            if(User.Identity.IsAuthenticated)
+            {
+                reservations = reservations.Where(s => s.UserId == customer && s.ResDate > curDate); //set the list of reservations to current user and only shows future reservations
+            }
+            else
+            {
+                //reservations = reservations.Where(s => s.UserId == ""); //might want to change this in the future
+            }
+            return View(await reservations.ToListAsync());
+            //return View(await _context.Reservations.ToListAsync());
+        }
+
+        // GET: Reservations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(m => m.ResId == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: Reservations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ResId,UserId,ResDate,PartySize")] Reservation reservation)
+        {
+            //System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            if (User.Identity.IsAuthenticated)
+            {
+                var customer = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                reservation.UserId = customer;
+            }
+            
+            if (ModelState.IsValid)
+            {
+                _context.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ResId,UserId,ResDate,PartySize")] Reservation reservation)
+        {
+            if (id != reservation.ResId)
+            {
+                return NotFound();
+            }
+            if (User.Identity.IsAuthenticated)
+            {
+                var customer = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                reservation.UserId = customer;
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.ResId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(m => m.ResId == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservations.Any(e => e.ResId == id);
+        }
+    }
+}
---
+++ TableReservation/Data/Migrations/20211025183359_updateReservation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TableReservation.Data.Migrations
+{
+    public partial class updateReservation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
